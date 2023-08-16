5771d7cb06c27847cd8e538b85b84a2177c0bebb
khushii123 <thakurkhushi@06005gmail>
2023-08-16 10:07:13 +0530

    add map file

---
+++ Map/2.Map.js
@@ -0,0 +1,8 @@
+const nums = [1, 2, 3, 4, 5, 6, 7];
+
+const multipleByTwo = nums.map((currentItem, index, arr) => {
+  console.log({ currentItem, index, arr });
+  return currentItem * 2;
+});
+
+console.log({ nums, multipleByTwo });
